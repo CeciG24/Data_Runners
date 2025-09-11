0310f76367b17ed1fff31a40d92b9bee27225927
leopar <leonardo.avalos.080104@gmail.com>
2025-09-11 00:35:00 -0600

    Fase 3 y 4 terminadas

--- frontend/src/components/NivelButton.jsx
+++ frontend/src/components/NivelButton.jsx
@@ -1,12 +1,22 @@
 // src/components/NivelButton.jsx
-export default function NivelButton({ numero, img, posicion, onClick }) {
+import { useState } from "react";
+export default function NivelButton({ numero, img, posicion, onClick, scale, shadowColor }) {
+  const [hovered, setHovered] = useState(false);
+
   return (
     <img
       src={img}
       alt={`Nivel ${numero}`}
       onClick={onClick}
-      className="absolute w-20 h-20 cursor-pointer hover:scale-170 hover:drop-shadow-[0_0_15px_rgba(212,175,55,0.9)] transition-transform transform scale-150 duration-300"
-      style={{ top: posicion.top, left: posicion.left }}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className={`absolute w-20 h-20 cursor-pointer hover:scale-170 transition-transform transform scale-150 duration-300 transition-filter`}
+      style={{
+        top: posicion.top,
+        left: posicion.left,
+        transform: `translate(-50%, -50%) scale(${scale})`,
+                filter: hovered && shadowColor ? `drop-shadow(${shadowColor})` : "none",
+      }}
     />
   );
 }
